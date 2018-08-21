fd34cc0baddaa6b1088e7c07cb3d8ffcf387e5ee
gloria <pureainu@gmail.com>
2018-08-21 10:18:09 +0900

    [Add] vue-styleguidist - caused by style document

---
+++ src/components/AppButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <button :style="styles" @click="handleClick">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+  export default {
+    name: "app-button",
+    props: {
+      color: {
+        type: String,
+        default: "black"
+      },
+      background: {
+        type: String,
+        default: "white"
+      }
+    },
+    computed: {
+      styles() {
+        return {
+          color: this.color,
+          background: this.background
+        };
+      }
+    },
+    methods: {
+      handleClick(e) {
+        this.$emit("click", e);
+      }
+    }
+  };
+</script>
