66cdf1ce91157b579c91e4390c789d49b214b3b9
gloriaJun <pureainu@gmail.com>
2018-08-21 15:06:08 +0900

    [Modify] vue-styleguidist doc - set code example style

--- styleguide.config.js
+++ styleguide.config.js
@@ -5,13 +5,8 @@ module.exports = {
   title: 'Gvu UI Component',
   sections: [
     {
-      name: 'Basic',
-      sections: [
-        {
-          name: 'Typography',
-          content: 'docs/basic/typography.md',
-        },
-      ],
+      name: 'Typography',
+      content: 'docs/Typography.md',
     },
     {
       name: 'Components',
@@ -21,6 +16,24 @@ module.exports = {
   getComponentPathLine(componentPath) {
     const name = path.basename(componentPath, '.vue');
     return `import { ${name} } from 'gvu';`;
+  },
+  highlightTheme: 'material',
+  styles: {
+    Markdown: {
+      code: {
+        padding: '0.2em 0.5em',
+        border: '1px solid rgba(0,0,0,0.05)',
+        margin: 0,
+        fontSize: '85%',
+        backgroundColor: 'rgba(27,31,35,0.05)',
+        borderRadius: '3px'
+      }
+    },
+    StyleGuide: {
+      content: {
+        maxWidth: '100%'
+      }
+    }
   },
   // webpackConfig: {
   //   // custom config goes here
