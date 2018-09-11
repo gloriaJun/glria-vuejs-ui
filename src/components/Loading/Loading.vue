58a9bfe08962f46847a21c53cdc0ec62345713ad
gloria <pureainu@gmail.com>
2018-09-11 23:45:30 +0900

    [Modify] loading component - default

--- src/components/Loading/Loading.vue
+++ src/components/Loading/Loading.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="vu-loading">
+  <div
+    v-if="show"
+    class="vu-loading">
+    <div class="loading-spinner">
+      <vu-icon
+        color="primary"
+        icon="spinner"
+        animation="spin"
+        class="loading-icon"></vu-icon>
+      <div
+        v-if="text"
+        class="loading-text text-primary">
+        {{ text }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +24,16 @@
 export default {
   name: 'VuLoading',
   props: {
+    /**
+     * is show loading
+     */
+    show: Boolean,
+    /**
+     * loading message
+     */
+    text: String,
+    directive: Boolean,
+    service: Boolean,
   },
   computed: {
     classes() {
@@ -17,7 +41,43 @@ export default {
       ];
     },
   },
+  mounted() {
+    if (!this.directive && !this.service) {
+      this.$parent.$el.style.position = 'relative';
+    }
+  },
   methods: {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.vu-loading {
+  position: absolute;
+  z-index: 2000;
+  background-color: rgba(0, 0, 0, 0.6);
+  margin: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transition: opacity .3s;
+
+  .loading-spinner {
+    top: 50%;
+    margin-top: -3em;
+    width: 100%;
+    text-align: center;
+    position: absolute;
+
+    .loading-icon {
+      font-size: 2.5em;
+    }
+    .loading-text {
+      margin: 3px 0;
+      font-weight: 600;
+      font-size: 1.2em;
+    }
+  }
+}
+</style>
--- stories/components/loading.stories.js
+++ stories/components/loading.stories.js
@@ -3,21 +3,23 @@ import VueInfoAddon from 'storybook-addon-vue-info';
 import { action } from '@storybook/addon-actions';
 import {
   boolean,
-  select,
   text,
 } from '@storybook/addon-knobs';
-
-import { COLOR_TYPES } from '../constants';
 
 storiesOf('Components|Loading', module)
   .addDecorator(VueInfoAddon)
   .add('default', () => {
+    const loadingText = text('loadingText', 'Loading...');
+    const loading = boolean('loading', true);
 
     return ({
       template: `
+      <div class="border border-danger" style="height: 200px;">
+        <vu-loading
+          :show="${loading}"
+          text="${loadingText}">
+        </vu-loading>
+      </div>
       `,
-      methods: {
-        onClick: () => action('click badge')(),
-      },
     });
   })
