f873f0d63e89a4acbee2632e4e5704bf63142a4f
gloria <pureainu@gmail.com>
2018-09-20 20:40:23 +0900

    [Add] input component - init

---
+++ src/components-form/Input/Input.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="vu-input">
+    <label
+      v-if="label"
+      :for="formId">
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script>
+import uuid from '../../utils/uuid';
+
+export default {
+  name: 'VuInput',
+  props: {
+    id: String,
+    value: [Boolean, Number, String, Array, Object],
+    disabled: Boolean,
+    label: String,
+  },
+  data() {
+    return {
+      formId: this.id || uuid(),
+      currentValue: this.value,
+    };
+  },
+  computed: {
+    classes() {
+      return [
+      ];
+    },
+  },
+  watch: {
+    value(newVal) {
+      this.currentValue = newVal;
+    },
+    currentValue(newVal) {
+      this.$emit('input', newVal);
+    },
+  },
+  methods: {
+  },
+};
+</script>
