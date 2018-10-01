de470ed2d73bc306077e74e93cd471c978ee98ee
gloria <pureainu@gmail.com>
2018-10-02 01:28:27 +0900

    [Modify] slider component - set min,max (temp)

--- src/components/Slider/Slider.vue
+++ src/components/Slider/Slider.vue
@@ -2,6 +2,18 @@
   <div
     :class="classes"
     class="vu-slider">
+    <div
+      ref="track"
+      class="range-track"
+      @click="handleClickSlider">
+      <div
+        :style="styleTrack"
+        :class="['range-track-fill', {dragging: dragging}]"/>
+    </div>
+    <vu-button
+      :style="styleThumb(firstValue)"
+      :class="['range-thumb', {dragging: dragging}]"
+      circle/>
   </div>
 </template>
 
@@ -43,17 +55,86 @@ export default {
   },
   data() {
     return {
+      firstValue: 0,
+      secondValue: 0,
+      dragging: false,
     };
   },
   computed: {
     classes() {
       return [
+        `slider-${this.color}`,
+        { disabled: this.disabled },
       ];
+    },
+    isRange() {
+      return (Array.isArray(this.value));
+    },
+    styleTrack() {
+      return this.isRange ?
+        {} :
+        {
+          width: `${this.calcPosition(this.firstValue)}%`,
+          left: '0%',
+        };
     },
   },
   watch: {
+    // firstValue(newVal) {
+    //   this.$emit('input', newVal);
+    // },
+  },
+  mounted() {
+    if (this.isRange) {
+      console.log('range');
+    } else {
+      this.firstValue = (this.value === null) ?
+        this.min : Math.min(this.max, Math.max(this.min, this.value));
+    }
   },
   methods: {
+    calcPosition(value) {
+      return 100 * ((value - this.min) / (this.max - this.min));
+    },
+    styleThumb(value) {
+      const style = { left: `${this.calcPosition(value)}%` };
+      if (this.dragging) {
+        style.cursor = 'grabbing';
+      }
+      return style;
+    },
+    /**
+     * @event - when clicked slider bar
+     * @param event
+     */
+    handleClickSlider(event) {
+      // get point of the track
+      const {
+        left: offsetLeft,
+        width: trackWidth,
+      } = this.$refs.track.getBoundingClientRect();
+      const offset = event.clientX - offsetLeft;
+      const percent = Math.round((offset / trackWidth) * 100);
+      const value = this.min + ((percent * (this.max - this.min)) / 100);
+
+      // if outside slider, set value to min or max
+      // if (value > this.max) value = this.max;
+      // else if (value < this.min) value = this.min;
+      this.setPosition(value);
+    },
+    /**
+     * set position of thumb
+     * @param position
+     */
+    setPosition(position) {
+      const stepLength = 100 / ((this.max - this.min) / this.step);
+      const steps = Math.round(position / stepLength);
+      const value = (steps * stepLength * (this.max - this.min) * 0.01) + this.min;
+
+      console.log(this.max, value);
+      // this.firstValue = parseFloat(value.toFixed(2));
+      this.$emit('input', parseFloat(value.toFixed(2)));
+    },
   },
 };
 </script>
--- stories/components-form/slider.stories.js
+++ stories/components-form/slider.stories.js
@@ -3,12 +3,15 @@ import VueInfoAddon from 'storybook-addon-vue-info';
 import { action } from '@storybook/addon-actions';
 import {
   boolean,
-  number,
+  number, select,
 } from '@storybook/addon-knobs';
+
+import { COLOR_TYPES } from '../constants';
 
 storiesOf('Components-Form|Slider', module)
   .addDecorator(VueInfoAddon)
   .add('default', () => {
+    const color = select('color', COLOR_TYPES, 'primary');
     const disabled = boolean('disabled', false);
     const value = number('value', 20);
     const min = number('min', 0);
@@ -25,6 +28,7 @@ storiesOf('Components-Form|Slider', module)
         <input type="number" min=0 max=100 v-model="value">
         <vu-slider
           v-model="value"
+          color="${color}"
           :min="${min}"
           :max="${max}"
           :disabled="${disabled}"></vu-slider>
