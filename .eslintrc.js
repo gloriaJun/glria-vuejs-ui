31a53dd555f284b626c1a42b94d71060891504f8
gloria <pureainu@gmail.com>
2018-10-09 15:52:38 +0900

    [Modify] package config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,14 @@ module.exports = {
   },
   extends: [
     'plugin:vue/recommended',
-    '@vue/airbnb',
+    // '@vue/airbnb',
+    "@vue/prettier"
   ],
   rules: {
     "vue/require-default-prop": 'off',
-    "no-param-reassign": [2, { "props": false }],
-    "no-underscore-dangle": 0,
-    "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+    // "no-param-reassign": [2, { "props": false }],
+    // "no-underscore-dangle": 0,
+    // "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
